99d6a6a85411d03e40a09f6bf1a31288b288244c
ArvinNotDev <arveenjafari19@gmail.com>
2024-09-03 14:07:33 +0330

    updated after big changes

--- digital_appointment/medicine/serializers.py
+++ digital_appointment/medicine/serializers.py
@@ -37,3 +37,13 @@ class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Service
         fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Comment
+        fields = ['id', 'provider', 'rating', 'name', 'email', 'body', 'created_on', 'is_active']
+        read_only_fields = ['id', 'created_on', 'is_active']
+
+    def create(self, validated_data):
+        return super().create(validated_data)
